5f8d0af04f32dc8646b3edb7f284c141fcea4c75
Boss <s.sittilerdpaisal@gmail.com>
2017-05-22 11:46:49 +0700

    First commit

---
+++ App/Containers/Styles/LaunchScreenStyles.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native'
+import { Metrics, ApplicationStyles } from '../../Themes/'
+
+export default StyleSheet.create({
+  ...ApplicationStyles.screen,
+  logo: {
+    marginTop: Metrics.doubleSection,
+    height: Metrics.images.logo,
+    width: Metrics.images.logo,
+    resizeMode: 'contain'
+  },
+  centered: {
+    alignItems: 'center'
+  },
+
+  selfView: {
+    width: 200,
+    height: 150,
+  },
+  remoteView: {
+    width: 200,
+    height: 150,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+    paddingBottom: Metrics.baseMargin
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  listViewContainer: {
+    height: 150,
+  },
+})
